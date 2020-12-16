95a71eb3d1ca978246aa47944f79e0531516709a
George Song <george@monozuku.com>
2020-12-16 08:23:18 -0800

    feat: add a simple Django view example

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Car, Driver
+
+
+def driver_detail(request, pk):
+    owner_obj = Driver.objects.get(pk=pk)
+    car_objs = Car.objects.filter(owner_id=owner_obj.id)
+    context = {
+        "vehicles": car_objs,
+        "drivers": owner_obj,
+    }
+    return render(request, "driver_detail.html", context)
